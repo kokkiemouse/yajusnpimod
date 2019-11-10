2643c032d0980dfb56aff312c65e9ac88637ddc8
kokkiemouse <kokkiemouse@gmail.com>
2019-11-10 21:51:23 +0900

    yaju

--- src/main/java/net/kokkiemouse/yaju/Blocks/OmaruBlock.java
+++ src/main/java/net/kokkiemouse/yaju/Blocks/OmaruBlock.java
@@ -12,26 +12,51 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+/**
+ *うんち出す奴の実装
+ * @author kokkiemouse
+ */
 public class OmaruBlock extends Block {
 
+    /**
+     *レンダータイプ指定!
+     * @param blockState_1 ブロック状態?
+     * @return
+     */
     @Environment(EnvType.CLIENT)
     public BlockRenderType getRenderType(BlockState blockState_1) {
         return BlockRenderType.MODEL;
     }
 
-
+    /**
+     *おまるのコンストラクタ
+     * @param block$Settings_1 設定
+     */
     public OmaruBlock(Settings block$Settings_1) {
         super(block$Settings_1);
     }
+
+    /**
+     *上に乗ったら脱糞だ!
+     * @param world ワールド
+     * @param blockpos 座標
+     * @param entity 上に乗ったエンティティ
+     */
     @Override
     public void onSteppedOn(World world, BlockPos blockpos, Entity entity){
         //System.out.println(entity.getName().toString());
         if(entity.isInSneakingPose()){
-            System.out.println("Sneeek!!" + blockpos.toString());
+            //System.out.println("Sneeek!!" + blockpos.toString());
 
             this.excretion(world,blockpos);
         }
     }
+
+    /**
+     *うんこしたら
+     * @param world ワールド
+     * @param blockpos ブロック座標
+     */
     public void excretion(World world, BlockPos blockpos)
     {
         if (!world.isClient) {
